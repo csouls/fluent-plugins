7ead4487fb838ac01dce1335a9a6d7899d7e7440
y.tanaka <y.tanaka@aktsk.jp>
2013-02-25 15:46:07 +0900

    Add in_tail data type extend plugin

---
+++ plugin/in_tail_extend.rb
@@ -0,0 +1,41 @@
+module Fluent
+
+  require 'fluent/plugin/in_tail'
+
+  class TailInputExtend < TailInput
+    Plugin.register_input('tail_ex', self)
+
+    config_param :int, :string, :default => ""
+    config_param :float, :string, :default => ""
+
+    def configure(conf)
+      super
+
+      @int = @int.split(',').map {|key| key.strip}
+      @float = @float.split(',').map {|key| key.strip}
+    end
+
+    def parse_line(line)
+      time, record = @parser.parse(line)
+      @int.each {|key|
+        next if !record.has_key?(key) || record[key].empty?
+        begin
+          value = Integer record[key]
+        rescue ArgumentError => e
+          puts e; next
+        end
+        record[key] = value
+      }
+      @float.each {|key|
+        next if !record.has_key?(key) || record[key].empty?
+        begin
+          value = Flaot record[key]
+        rescue ArgumentError => e
+          puts e; next
+        end
+        record[key] = value
+      }
+      return time, record
+    end
+  end
+end
